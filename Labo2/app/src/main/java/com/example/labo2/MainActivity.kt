c26600481a67c82ad197a5cd14fdc80a5275e042
Alejo <alejo7d1@gmail.com>
2026-03-27 18:32:29 -0600

    Fixed: labo2 interface

--- Labo2/app/src/main/java/com/example/labo2/MainActivity.kt
+++ Labo2/app/src/main/java/com/example/labo2/MainActivity.kt
@@ -9,13 +9,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
@@ -28,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.labo2.ui.theme.Labo2Theme
@@ -50,6 +54,8 @@ fun mainScreen(){
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .padding(15.dp)
+            .statusBarsPadding()
             .background(Color.White),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
@@ -66,24 +72,45 @@ fun mainScreen(){
         }
         Row(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp),
+                .padding(10.dp)
+                .fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Text("Listado de nombre y posición en la lista")
-            Button({}) {
+            Text(
+                text = "Listado de nombre y posición en la lista",
+                modifier = Modifier.weight(1f)
+            )
+
+            Button(
+                modifier = Modifier.padding(12.dp,0.dp),
+                onClick = {
+                    nombresTabla.clear()
+                }
+            ) {
                 Text("Limpiar")
             }
         }
         LazyColumn(
             modifier = Modifier
-                .border(width = 1.dp, color = Color.Blue, shape = CircleShape)
+                .border(width = 5.dp, color = Color.Blue)
+                .padding(10.dp)
                 .fillMaxWidth()
                 .fillMaxHeight()
         ){
             itemsIndexed(nombresTabla) { index, item ->
-                Text(item)
-                Text(index.toString())
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = item
+                    )
+                    Text(
+                        text = (index + 1).toString()
+                    )
+                }
             }
         }
 
